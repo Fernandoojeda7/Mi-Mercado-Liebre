84823ccc94b2e222756e602a804480e826e698f3
Fernando Ojeda <fernandoojeda7@gmail.com>
2021-12-17 10:39:29 -0300

    proyecto Mercado Liebre terminado

---
+++ app.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath));
+
+
+app.listen(3000, ()=> {
+    console.log('El servidor esta funcionando en el puerto 3000')
+});
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+app.get('/login', (req, res)=> {
+    res.sendFile(path.resolve('./views/login.html'));
+})
+app.get('/register', (req, res)=> {
+    res.sendFile(path.resolve('./views/register.html'));
+})
